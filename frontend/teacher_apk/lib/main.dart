1b1d27c97887a63709f156ba4cd28aea356b6805
aritraghub2005 <aritraind06@gmail.com>
2025-08-15 12:02:00 +0530

    Loading Screen Completed

--- frontend/teacher_apk/lib/main.dart
+++ frontend/teacher_apk/lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'screens/login.dart';
+import 'screens/loading.dart';
 
 void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    initialRoute: 'login',
+    initialRoute: 'loading',
     routes: {
+      'loading': (context) => const LoadingScreen(),
       'login': (context) => const ArcanumLogin()
+
     },
   ));
 }
